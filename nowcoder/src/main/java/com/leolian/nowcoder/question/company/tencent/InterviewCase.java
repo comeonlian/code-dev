73d6677bdd07a73a5c4f0d3d9c40811e77ed22d2
LeoLian <lianliang2014@126.com>
2021-06-25 20:49:01 +0800

    【2021-06-25 20:48】1.commit updated code

---
+++ nowcoder/src/main/java/com/leolian/nowcoder/question/company/tencent/InterviewCase.java
@@ -0,0 +1,46 @@
+package com.leolian.nowcoder.question.company.tencent;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 已知搜索二叉树根节点root，求p、q节点的最近公共祖先
+ * struct TreeNode
+ * {
+ * TreeNode* left;
+ * TreeNode* right;
+ * int val;
+ * }
+ */
+public class InterviewCase {
+    public static void main(String[] args) {
+    }
+    private List<TreeNode> pListPath = new LinkedList<>();
+    
+    private List<TreeNode> qListPath = new LinkedList<>();
+    
+    public void getTreeNode(TreeNode root, TreeNode p, TreeNode q) {
+        if (null != root.left) {
+            getTreeNode(root.left, p, q);
+        }
+        if (null != root.right) {
+            getTreeNode(root.right, p, q);
+        }
+        if (root.val == p.val) {
+            pListPath.add(root);
+            return;
+        } else {
+            pListPath.add(root);
+        }
+        if (root.val == q.val) {
+            qListPath.add(root);
+        }
+    }
+
+    class TreeNode {
+        TreeNode left;
+        TreeNode right;
+        int val;
+    }
+
+}
