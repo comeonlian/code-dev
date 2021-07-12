fb5b66b3e3b24e7a72fdba2ae6506b4915dc264c
LeoLian <lianliang2014@126.com>
2021-07-12 22:19:41 +0800

    1.commit update code

--- nowcoder/src/main/java/com/leolian/nowcoder/question/bank/NC_0090.java
+++ nowcoder/src/main/java/com/leolian/nowcoder/question/bank/NC_0090.java
@@ -1,10 +1,13 @@
 package com.leolian.nowcoder.question.bank;
 
+import java.util.Stack;
+
 /**
  * NC 90 : 包含min函数的栈
  * <p>
  * 题目描述
- * 定义栈的数据结构，请在该类型中实现一个能够得到栈中所含最小元素的min函数，并且调用 min函数、push函数 及 pop函数 的时间复杂度都是 O(1)
+ * 定义栈的数据结构，请在该类型中实现一个能够得到栈中所含最小元素的min函数，
+ * 并且调用 min函数、push函数 及 pop函数 的时间复杂度都是 O(1)
  * push(value):将value压入栈中
  * pop():弹出栈顶元素
  * top():获取栈顶元素
@@ -28,22 +31,43 @@ public class NC_0090 {
     public static void main(String[] args) {
         NC_0090 nc0090 = new NC_0090();
 
+        nc0090.push(-1);
+        nc0090.push(2);
+        System.out.println(nc0090.min());
+        System.out.println(nc0090.top());
+        nc0090.pop();
+        nc0090.push(1);
+        System.out.println(nc0090.top());
+        System.out.println(nc0090.min());
     }
 
-    public void push(int node) {
+    private Stack<Integer> dataStack = new Stack<>();
+    private Stack<Integer> minStack = new Stack<>();
 
+    public void push(int node) {
+        dataStack.push(node);
+        if (minStack.size() == 0) {
+            minStack.push(node);
+        } else {
+            int min = minStack.peek();
+            if (min > node) {
+                min = node;
+            }
+            minStack.push(min);
+        }
     }
 
     public void pop() {
-
+        dataStack.pop();
+        minStack.pop();
     }
 
     public int top() {
-
+        return dataStack.peek();
     }
 
     public int min() {
-
+        return minStack.peek();
     }
 
 }
