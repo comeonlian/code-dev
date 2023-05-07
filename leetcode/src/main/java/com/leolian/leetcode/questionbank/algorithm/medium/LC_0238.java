bba31de2d6862f29528cdf76d5dc5d2a7c9b45e6
LeoLian <lianliang2014@126.com>
2023-05-07 16:18:13 +0800

    commit code.

---
+++ leetcode/src/main/java/com/leolian/leetcode/questionbank/algorithm/medium/LC_0238.java
@@ -0,0 +1,60 @@
+package com.leolian.leetcode.questionbank.algorithm.medium;
+
+import java.util.Arrays;
+
+/**
+ * LC 238 : 除自身以外数组的乘积
+ * 给你一个整数数组nums，返回 数组answer，其中answer[i]等于nums中除nums[i]之外其余各元素的乘积。
+ * 题目数据 保证 数组nums之中任意元素的全部前缀元素和后缀的乘积都在  32 位 整数范围内。
+ * 请不要使用除法，且在O(n) 时间复杂度内完成此题。
+ * 提示：
+ * 2 <= nums.length <= 105
+ * -30 <= nums[i] <= 30
+ * 保证 数组nums之中任意元素的全部前缀元素和后缀的乘积都在 32 位 整数范围内
+ * 进阶：你可以在 O(1) 的额外空间复杂度内完成这个题目吗？（ 出于对空间复杂度分析的目的，输出数组不被视为额外空间。）
+ * <p>
+ * 示例1
+ * 输入: nums = [1,2,3,4]
+ * 输出: [24,12,8,6]
+ * <p>
+ * 示例2
+ * 输入: nums = [-1,1,0,-3,3]
+ * 输出: [0,0,9,0,0]
+ */
+public class LC_0238 {
+
+    public static void main(String[] args) {
+        LC_0238 lc0238 = new LC_0238();
+
+        int[] nums = new int[]{1,2,3,4};
+        int[] result = lc0238.productExceptSelf(nums);
+        System.out.println(Arrays.toString(result));
+    }
+
+    public int[] productExceptSelf(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return nums;
+        }
+        int[] left = new int[nums.length];
+        left[0] = 1;
+        for (int i = 1; i < nums.length; i++) {
+            left[i] = left[i - 1] * nums[i - 1];
+        }
+        int[] right = new int[nums.length];
+        right[nums.length - 1] = 1;
+        for (int i = nums.length - 2; i >= 0; i--) {
+            right[i] = right[i + 1] * nums[i + 1];
+        }
+        int[] answer = new int[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            answer[i] = left[i] * right[i];
+        }
+        return answer;
+    }
+
+    /*
+    执行用时：1 ms, 在所有 Java 提交中击败了100.00%的用户
+    内存消耗：52.5 MB, 在所有 Java 提交中击败了5.00%的用户
+    通过测试用例：22 / 22
+     */
+}
