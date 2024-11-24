796a79f482467c384912eb9dd39ea867f57f2c21
LeoLian <lianliang2014@126.com>
2024-11-24 15:50:57 +0800

    commit code

---
+++ leetcode/src/main/java/com/leolian/leetcode/questionbank/algorithm/page05/LC_0233.java
@@ -0,0 +1,29 @@
+package com.leolian.leetcode.questionbank.algorithm.page05;
+
+/**
+ * LC 233 : 数字 1 的个数
+ * 给定一个整数 n，计算所有小于等于 n 的非负整数中数字 1 出现的个数。
+ * 提示：
+ * 0 <= n <= 10^9
+ * <p>
+ * 示例1
+ * 输入：n = 13
+ * 输出：6
+ * <p>
+ * 示例2
+ * 输入：n = 0
+ * 输出：0
+ */
+public class LC_0233 {
+
+    public static void main(String[] args) {
+        LC_0233 lc0233 = new LC_0233();
+
+        int n = 13;
+        System.out.println(lc0233.countDigitOne(n));
+    }
+
+    public int countDigitOne(int n) {
+        return 0;
+    }
+}
